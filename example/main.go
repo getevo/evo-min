1e92ce6d819b5d4baf8585cb49ebe34cc2d2c722
RezaPC <reza@ies-italia.it>
2023-03-08 17:43:53 +0100

    log

--- example/main.go
+++ example/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/getevo/evo-min"
+import (
+	"github.com/getevo/evo-min"
+	"github.com/getevo/evo-min/lib/log"
+	"github.com/getevo/evo-min/lib/settings"
+)
 
 func main() {
 	evo.Setup()
 	var db = evo.GetDBO()
 	_ = db
+	log.Error("Application has been started", "http", settings.Get("HTTP.WriteTimeout").String(), "bool", true)
+	log.SetLevel(log.DebugLevel)
 	evo.Run()
 }
